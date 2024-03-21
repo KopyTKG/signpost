a874323387543e355584c907e0b8e0cc293c822c
Martin Kopecký <kopecky@thekrew.app>
2024-03-21 21:59:46 +0100

    Development (#24)
    * First take on #22 issue
    
    * moving to shadcn and mongo backend with prisma
    
    * framer added with link base
    
    * removing nextui
    
    ---------
    
    Co-authored-by: Martin Kopecky <kopecky.martin259@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
+import { Link, LinkIcon } from '@/components/slot'
 import Image from 'next/image'
-import ProfileLoading from '../components/fallback/loading.profile'
-import { Suspense } from 'react'
-import LinksLoading from '@/components/fallback/loading,links'
 
 export default function Home() {
-  return (
-    <main>
-      <div className='flex center flex-column gap-2'>
-        <Suspense fallback={<ProfileLoading />}>
-          <ProfileLoading />
-        </Suspense>
-        <Suspense fallback={<LinksLoading />}>
-          <LinksLoading />
-        </Suspense>
-      </div>
-    </main>
-  )
+ return (
+  <>
+   <div className="w-screen flex justify-center py-10">
+    <div className="flex flex-col justify-center items-center">
+     <div className="w-24 h-24 border-2 rounded-full overflow-hidden">
+      <Image
+       // random cat picture (png)
+       src={'https://picsum.photos/200'}
+       alt="logo"
+       width={200}
+       height={200}
+      />
+     </div>
+     <h1 className="text-2xl font-bold text-slate-200">@Signpost</h1>
+    </div>
+   </div>
+   <div className="max-w-3xl mx-auto px-5 flex flex-col gap-5">
+    <Link color="light" radius="soft" className="flex justify-start">
+     <LinkIcon radius="soft" />
+    </Link>
+    <Link color="light" radius="soft" className="flex justify-start">
+     <LinkIcon radius="soft" />
+    </Link>
+    <Link color="light" radius="soft" className="flex justify-start">
+     <LinkIcon radius="soft" />
+    </Link>
+   </div>
+  </>
+ )
 }
