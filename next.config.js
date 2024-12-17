4f9c5d1e03e0e94d977356e33b976dd7b4ecf9d7
Martin Kopecký <kopecky@thekrew.app>
2024-12-17 16:04:06 +0100

    Development CSP update (#29)
    * v3.1.0
    
    * adding https headers
    
    * more sctrict headers
    
    * switching to module
    
    * eh
    
    * problem with headers
    
    * maybe working CSP
    
    * more CSP
    
    * my bad wrong URL in CSP

--- next.config.js
+++ next.config.js
@@ -13,6 +13,59 @@ const nextConfig = {
    },
   ],
  },
+
+ eslint: {
+  ignoreDuringBuilds: true,
+ },
+ async headers() {
+  return [
+   {
+    source: '/:path*',
+    headers: [
+     {
+      key: 'Access-Control-Allow-Origin',
+      value: process.env.DOMAIN || '*',
+     },
+     {
+      key: 'Access-Control-Allow-Methods',
+      value: 'GET, OPTIONS',
+     },
+     {
+      key: 'Access-Control-Allow-Headers',
+      value: 'Content-Type, Authorization',
+     },
+     {
+      key: 'X-Frame-Options',
+      value: 'DENY',
+     },
+     {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff',
+     },
+     {
+      key: 'Referrer-Policy',
+      value: 'strict-origin-when-cross-origin',
+     },
+     {
+      key: 'Permissions-Policy',
+      value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+     },
+     {
+      key: 'Cache-Control',
+      value: 'public, max-age=0, must-revalidate',
+     },
+     {
+      key: 'Strict-Transport-Security',
+      value: 'max-age=63072000; includeSubDomains; preload',
+     },
+     {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block',
+     },
+    ],
+   },
+  ]
+ },
 }
 
-module.exports = nextConfig
+export default nextConfig
