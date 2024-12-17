277bf35dfb5686b655092cad5ba3e972a36db9df
Martin Kopecký <kopecky@thekrew.app>
2024-12-17 15:55:39 +0100

    more CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
 
  const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://va.vercel-scripts.com;
+    script-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://va.vercel-scripts.com https://va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: https:;
     font-src 'self';
